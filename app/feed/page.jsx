7da15779af720a1921208fe46e21695e11a68b1b
V-Vimal-kumar <vimalthewarrior25@gmail.com>
2025-12-26 10:40:51 +0530

    final

--- app/feed/page.jsx
+++ app/feed/page.jsx
@@ -1,15 +1,7 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import AppLayout from "@/components/layout/AppLayout";
 import FeedLayout from "@/components/layout/FeedLayout";
 
 export default function FeedPage() {
-  const token = cookies().get("token")?.value;
-
-  if (!token) {
-    redirect("/login");
-  }
-
   return (
     <AppLayout>
       <FeedLayout />
